b6aafa0cd5d18a2e5d497cfdbbc85736c6c9ab08
tengkuzulfadli <nomad.plus@yahoo.com>
2022-01-01 12:44:45 +1100

    Upload Json to Firebase

---
+++ config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDVqZjD8PL8C7qOfKwvbrW35Q0H5_qpXj4",
+  authDomain: "portfolio-zul.firebaseapp.com",
+  projectId: "portfolio-zul",
+  storageBucket: "portfolio-zul.appspot.com",
+  messagingSenderId: "97804193395",
+  appId: "1:97804193395:web:3673771567c5f8e82a6406",
+  measurementId: "G-WB8739WFQB"
+};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
+module.exports = firebaseConfig;
