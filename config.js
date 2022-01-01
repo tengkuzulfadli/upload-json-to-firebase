65f752a4890dc82684198207a716f7e6d424e9e3
Tengku Zulfadli <62935899+tengkuzulfadli@users.noreply.github.com>
2022-01-01 12:55:24 +1100

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDVqZjD8PL8C7qOfKwvbrW35Q0H5_qpXj4",
+  apiKey: "",
   authDomain: "portfolio-zul.firebaseapp.com",
   projectId: "portfolio-zul",
   storageBucket: "portfolio-zul.appspot.com",
-  messagingSenderId: "97804193395",
-  appId: "1:97804193395:web:3673771567c5f8e82a6406",
-  measurementId: "G-WB8739WFQB"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
